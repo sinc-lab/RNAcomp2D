b0c3264509bc945bb94c38f24e23f4bc3ff59f61
Rosario Vitale <rvitale@sinc.unl.edu.ar>
2025-07-24 21:23:39 -0300

    Fix some issues.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, session, Response, url_for
+from flask import Flask, render_template, request, redirect, Response, url_for
+import secrets
 from markupsafe import Markup
 import os, shutil
 import json
@@ -9,9 +10,14 @@ import time
 app = Flask(__name__)
 app_root = '/rna2dstructure'
 app.config['APPLICATION_ROOT'] = app_root
-app.secret_key = 'some_secret_key_for_session'
 
-# Middleware para ajustar SCRIPT_NAME
+SESSIONS = {}
+BASE_TEMP_DIR = 'sessions_temp'
+if os.path.exists(BASE_TEMP_DIR):
+    shutil.rmtree(BASE_TEMP_DIR)
+os.makedirs(BASE_TEMP_DIR, exist_ok=True)
+
+# Middleware to set SCRIPT_NAME
 class PrefixMiddleware:
     def __init__(self, app, prefix):
         self.app = app
@@ -22,7 +28,7 @@ class PrefixMiddleware:
         return self.app(environ, start_response)
 
 
-# Envolver la app con el middleware (IMPORTANTE) 
+# Apply the middleware
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=app_root)
 
 # Thread configuration: partial parallelization
@@ -79,7 +85,7 @@ def createMethodsList(names, methods):
     return cln_sel_met_thr
 
 
-def runMethods(seq, methods):
+def runMethods(seq, methods, session_id):
     """Run the methods in a separate thread.
 
     :param seq: sequence to be folded
@@ -87,6 +93,7 @@ def runMethods(seq, methods):
 
     """
     #print("running methods")
+    TEMP_DIR = os.path.join(BASE_TEMP_DIR, session_id)
     methods_list = list(methods.keys())
     if "Reference" in methods_list:
         methods_list.remove("Reference")
@@ -94,10 +101,10 @@ def runMethods(seq, methods):
     for method in methods_list:
         params = methods[method]
         #print(f"running {method}")
-        with open(f"temps/{method}_status.txt", "w") as f:
+        with open(f"{TEMP_DIR}/temps/{method}_status.txt", "w") as f:
             f.write("running")
         start = time.time()
-        val = getattr(mt, method).run_method(seq, params)
+        val = getattr(mt, method).run_method(seq, params, TEMP_DIR)
         end = time.time()
         # Append method stats
         #with open(f"stats/{method}.csv", "a") as f:
@@ -105,10 +112,10 @@ def runMethods(seq, methods):
         #print(f"*** Method {method} took {end - start} seconds ***")
         #print(f"Method {method} returned {val}")
         if val == "OK":
-            with open(f"temps/{method}_status.txt", "w") as f:
+            with open(f"{TEMP_DIR}/temps/{method}_status.txt", "w") as f:
                 f.write("OK")
         else:
-            with open(f"temps/{method}_status.txt", "w") as f:
+            with open(f"{TEMP_DIR}/temps/{method}_status.txt", "w") as f:
                 f.write("Error: " + val)
 
 
@@ -124,56 +131,73 @@ def show_results():
     """Redirects to the results page.
     """
     if request.method == 'POST':
-        print("Received data:", request.json)
+        #print("Received data:", request.json)
         seq = request.json["sequence"]
         methods = request.json["methods"]
         secondary_structure = request.json["secondary_structure"]
-        session["seq"] = seq
-        session["methods"] = methods
-        session["names"] = list(methods.keys())
-        session["rnacentral_id"] = request.json["rnacentral_id"]
+        session_id = secrets.token_hex(16)
+        #print("Session ID:", session_id)
+
+        os.makedirs(f"{BASE_TEMP_DIR}/{session_id}", exist_ok=True)
+        os.makedirs(f"{BASE_TEMP_DIR}/{session_id}/temps", exist_ok=True)
+        os.makedirs(f"{BASE_TEMP_DIR}/{session_id}/results", exist_ok=True)
+
+        SESSIONS[session_id] = {}
+        SESSIONS[session_id]["seq"] = seq
+        SESSIONS[session_id]["methods"] = methods
+        SESSIONS[session_id]["names"] = list(methods.keys())
+        SESSIONS[session_id]["rnacentral_id"] = request.json["rnacentral_id"]
         if secondary_structure!="":
-            session["names"] = ["Reference"] + list(methods.keys())
-            session["methods"]["Reference"] = {}
-            with open("results/Reference.dot", "w") as f:
+            SESSIONS[session_id]["names"] = ["Reference"] + list(methods.keys())
+            SESSIONS[session_id]["methods"]["Reference"] = {}
+            with open(f"{BASE_TEMP_DIR}/{session_id}/results/Reference.dot", "w") as f:
                 f.write("Reference\n")
                 f.write(seq + "\n")
                 f.write(secondary_structure + "\n")
-        session.permanent = True
-        return redirect(url_for('results'))
+        return redirect(url_for('results', session_id=session_id))
 
 
-@app.route('/results')
-def results():
+@app.route('/results/<session_id>')
+def results(session_id):
     """Renders the results page with session data.
     """
-    print("Session data:", session)
+    session = SESSIONS.get(session_id)
+    if not session:
+        return "Session expired or invalid", 404
     return render_template('show_results.html', seq=session["seq"],
                            methods=session["names"], 
-                           rnacentral_id=session["rnacentral_id"])
+                           rnacentral_id=session["rnacentral_id"],
+                           session_id=session_id)
 
 
-@app.route('/stream_results')
-def stream_results():
-    def generate_results(seq, methods):
-        basepath = "results/"
+@app.route('/stream_results/<session_id>')
+def stream_results(session_id):
+    session = SESSIONS.get(session_id)
+    if not session:
+        return "Session expired or invalid", 404
+
+    def generate_results():
+        seq = session["seq"]
+        methods = session["methods"]
+        #print("Generating results for session", session_id)
+        basepath = f"{BASE_TEMP_DIR}/{session_id}/results/"
         names = list(methods.keys())
         met_list = createMethodsList(names, methods)
         for m in names:
-            with open(f"temps/{m}_status.txt", "w") as f:
+            with open(f"{BASE_TEMP_DIR}/{session_id}/temps/{m}_status.txt", 
+                      "w") as f:
                 f.write("pending")
-        threads = []
+        SESSIONS[session_id]["threads"] = []
         for i in range(len(met_list)):
-            threads.append(threading.Thread(target=runMethods, 
-                                            args=(seq, met_list[i])))
-        for t in threads:
+            SESSIONS[session_id]["threads"].append(threading.Thread(target=runMethods, args=(seq, met_list[i], session_id)))
+        for t in SESSIONS[session_id]["threads"]:
             t.start()
         stop_while = False
         while not stop_while:
             data = {}
             count = 0
             for name in names:
-                with open(f"temps/{name}_status.txt", "r") as f:
+                with open(f"{BASE_TEMP_DIR}/{session_id}/temps/{name}_status.txt", "r") as f:
                     status = f.read()
                 if len(status) == 0:
                     #print(f"Status of {name} is {status} (empty)")
@@ -205,14 +229,13 @@ def stream_results():
                     else:
                         svg = "not found"
                     if os.path.exists(f"{basepath}{name}_circ.svg"):
-                        #print(f"Found {basepath}{name}_circ.svg")
                         circ = open(f"{basepath}{name}_circ.svg").read()
                         circ = Markup(circ)
                         #shutil.copyfile(f"{basepath}{name}_circ.svg", 
                         #                f"methods/svg/{name}_circ.svg")
                     data[name] = {"svg":svg, "status":"done", "dot":dot, 
                                   "circ":circ}
-                    with open(f"temps/{name}_status.txt", "w") as f:
+                    with open(f"{BASE_TEMP_DIR}/{session_id}/temps/{name}_status.txt", "w") as f:
                         f.write("sent")
                     for item in os.listdir(basepath):
                         if name in item:
@@ -235,7 +258,7 @@ def stream_results():
                         circ = "not found"
                     data[name] = {"svg": svg, "status": status, "dot": dot, 
                                   "circ": circ}
-                    with open(f"temps/{name}_status.txt", "w") as f:
+                    with open(f"{BASE_TEMP_DIR}/{session_id}/temps/{name}_status.txt", "w") as f:
                         f.write("sent")
                     for item in os.listdir(basepath):
                         if name in item:
@@ -251,24 +274,26 @@ def stream_results():
             #else:
                 #print(f"Waiting for {len(names) - count} methods to finish")
         #print("Joining threads")
-        for t in threads:
+        for t in SESSIONS[session_id]["threads"]:
             t.join()
         #with open(f"stats/total.csv", "a") as f:
         #    f.write(f"{config},{len(seq)},{end_time - start_time}\n")
         #print(f"*** Total time: {end_time - start_time} seconds ***")
         #print("Removing temp files")
-        for item in os.listdir("temps/"):
-            os.remove(f"temps/{item}")
-        #print("Removing result files")
-        for item in os.listdir("results/"):
-            os.remove(f"results/{item}")
+        shutil.rmtree(f"{BASE_TEMP_DIR}/{session_id}", ignore_errors=True)
+        SESSIONS.pop(session_id)
+        #for item in os.listdir("temps/"):
+        #    os.remove(f"temps/{item}")
+        ##print("Removing result files")
+        #for item in os.listdir("results/"):
+        #    os.remove(f"results/{item}")
         return
 
-    resp = Response(generate_results(session["seq"], session["methods"]), 
+    resp = Response(generate_results(),
                     mimetype='text/event-stream', 
                     content_type="text/event-stream")
-    resp.set_cookie("session", "session", max_age=60*60*24*365, secure=True,
-                    httponly=True, samesite="None")
+    #resp.set_cookie("session", "session", max_age=60*60*24*365, secure=True,
+    #                httponly=True, samesite="None")
     return resp
 
 
@@ -281,3 +306,4 @@ if __name__ == '__main__':
     port = 8000
     debug = True
     app.run(host=addr, port=port, debug=debug, use_reloader=False)
+
